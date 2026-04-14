19aa4ff6448ebed38cff2baacea29758c0bc2900
gusta-franca <gustavomartins5820@gmail.com>
2026-04-14 10:57:34 -0300

    Adapt bucketing_simd hash implementation from Bother, 2023

---
+++ include/hashmap/hashes/hasher.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <cmath>
+#include <cstdint>
+#include <functional>
+
+#include "hashmap/utils.hpp"
+#include "hedley.h"
+
+namespace hashmap::hashing {
+
+template <typename KeyT, bool use_modulo>
+struct Hasher {
+ public:
+  Hasher(uint64_t maximum_value) : maximum_value_{maximum_value} {
+    // we allow zero here as we sometimes (chained hash map need a garbage initializer)
+    ASSERT(utils::is_power_of_two(maximum_value) || maximum_value == 0 || use_modulo,
+           "When not using modulo-finalization, we need to map into the range [0; 2^w]. use_modulo = " +
+           std::to_string(use_modulo) + ", maximum_value = " + std::to_string(maximum_value) + ", next po2 = " + 
+           std::to_string(utils::value_or_np2(maximum_value)) + ".");
+  }
+
+  HEDLEY_ALWAYS_INLINE uint64_t finalize(const uint64_t& hash) const {
+    if constexpr (use_modulo) {
+      return hash % maximum_value_;
+    } else {
+      return hash & (maximum_value_ - 1);
+    }
+  }
+
+  std::string get_identifier() const {
+    if constexpr (use_modulo) {
+      return "UndefinedModHasher";
+    } else {
+      return "UndefinedBitHasher";
+    }
+  }
+
+  alignas(utils::cacheline_size) const uint64_t maximum_value_;
+};
+
+}  // namespace hashmap::hashing
